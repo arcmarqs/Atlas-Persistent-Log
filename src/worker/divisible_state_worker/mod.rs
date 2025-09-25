ccfd5004a421133024f45fa5dd47e80591259f0d
Amadeu Marques <maramadeu@gmail.com>
2025-09-25 11:05:43 +0100

    typo

--- src/worker/divisible_state_worker/mod.rs
+++ src/worker/divisible_state_worker/mod.rs
@@ -254,7 +254,7 @@ fn write_state_descriptor<S: DivisibleState>(db: &KVDB, descriptor: &S::StateDes
 
     serialize_state_descriptor::<Vec<u8>, S>(&mut value, &descriptor)?;
     
-    println!("writing descriptor}");
+    println!("writing descriptor");
     db.set(COLUMN_FAMILY_STATE, LATEST_STATE_DESCRIPTOR, value)?;
 
     Ok(())
